3bce2d1b131429ba410449b0809063030a69d4c7
wuqinghai01 <676374513@qq.com>
2020-06-19 19:40:15 +0800

    代码提交

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/wuqinghai/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,20 @@
+package com.wuqinghai.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName NacosConfigClientMain3377
+ * @Description TODO
+ * @Author wuqinghai01
+ * @Date 2020/6/19 15:06
+ * Version 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/wuqinghai/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package com.wuqinghai.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName ConfigClientController
+ * @Description TODO
+ * @Author wuqinghai01
+ * @Date 2020/6/19 15:07
+ * Version 1.0
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
